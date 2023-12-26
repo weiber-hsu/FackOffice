4d12449ec48f534f25f1e1255338f7ea270480af
weber.hsu53 <weber.hsu@titansoft.com.sg>
2023-12-26 20:07:47 +0800

    feat(Connect DB): Add connection

--- FakeOffice/Program.cs
+++ FakeOffice/Program.cs
@@ -1,11 +1,21 @@
 using FakeOffice.Controllers;
 using FakeOffice.Repository;
 using FakeOffice.Service;
+using MySql.Data.MySqlClient;
+using NSubstitute.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Configuration.AddJsonFile("appsettings.json");
+builder.Configuration.Bind("Database");
+string connectionString = builder.Configuration.GetSection("Database:ConnectionString").Value;
+builder.Services.AddScoped<MySqlConnection>(_ => new MySqlConnection(connectionString));
+
 builder.Services.AddControllers().AddApplicationPart(typeof(RegisterController).Assembly);
 builder.Services.AddTransient<IRegisterService, RegisterService>();
 builder.Services.AddTransient<IMemberRepo, MemberRepo>();
+
 var app = builder.Build();
 app.UseRouting();
 
