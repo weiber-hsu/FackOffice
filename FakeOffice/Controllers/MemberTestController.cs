ef1ec90334fdaa82440502f1edd39c658b18e42b
weber.hsu53 <weber.hsu@titansoft.com.sg>
2023-12-26 19:03:25 +0800

    feat(Testing API): create testing API controller

---
+++ FakeOffice/Controllers/MemberTestController.cs
@@ -0,0 +1,23 @@
+﻿using FakeOffice.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FakeOffice.Controllers;
+
+//may add white list for internal testing security
+[ApiController]
+[Route("api")]
+public class MemberTestController : ControllerBase
+{
+    private IMemberRepo _memberRepo;
+
+    public MemberTestController(IMemberRepo memberRepo)
+    {
+        _memberRepo = memberRepo;
+    }
+
+    [HttpGet]
+    public IActionResult GetMember(int memberId)
+    {
+        return Ok();
+    }
+}
