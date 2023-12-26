04a57e954950e39b4eeb7e4922e7130bc2a85381
weber.hsu53 <weber.hsu@titansoft.com.sg>
2023-12-26 21:17:39 +0800

    chore: add await to fix test

--- FakeOfficeUnitTests/RegisterControllerTests.cs
+++ FakeOfficeUnitTests/RegisterControllerTests.cs
@@ -21,9 +21,9 @@ public class RegisterControllerTests
     }
 
     [Test]
-    public void call_register_with_data_will_return_ok_with_success()
+    public async Task call_register_with_data_will_return_ok_with_success()
     {
-        var actionResult = _registerController.Register(new RegisterRequest());
+        var actionResult = await _registerController.Register(new RegisterRequest());
         var result = actionResult as OkObjectResult;
         actionResult.Should().BeOfType<OkObjectResult>();
         result?.Value.Should().BeEquivalentTo("Success");
