79074276c46f962bfb64feab6b0bb8d1836560e2
weber.hsu53 <weber.hsu@titansoft.com.sg>
2023-12-26 18:52:59 +0800

    feat(register): tdd green ,modify test case with param and refactor

--- FakeOffice/Models/RegisterDto.cs
+++ FakeOffice/Models/RegisterDto.cs
@@ -5,4 +5,13 @@ public class RegisterDto
     public string? Recommend { get; set; }
 
     public string? UserName { get; set; }
+
+    public Member ToMember()
+    {
+        return new Member()
+        {
+            UserName = UserName,
+            Recommend = Recommend
+        };
+    }
 }
